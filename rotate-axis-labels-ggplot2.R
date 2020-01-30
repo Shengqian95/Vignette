8d86856554f0bf40c72c9c12ccfd5fe16f8f3605
Sheng-Hu Qian <qians@webmail.hzau.edu.cn>
2020-01-30 09:58:35 +0100

    Create rotate-axis-labels-ggplot2.R

---
+++ rotate-axis-labels-ggplot2.R
@@ -0,0 +1,13 @@
+td <- expand.grid(
+    hjust=c(0, 0.5, 1),
+    vjust=c(0, 0.5, 1),
+    angle=c(0, 45, 90),
+    text="text"
+)
+ggplot(td, aes(x=hjust, y=vjust)) + 
+    geom_point(colour = "red",
+             size = 5) +
+    geom_text(aes(label=text, angle=angle, hjust=hjust, vjust=vjust)) + 
+    facet_grid(~angle) +
+    scale_x_continuous(breaks=c(0, 0.5, 1), expand=c(0, 0.2)) +
+    scale_y_continuous(breaks=c(0, 0.5, 1), expand=c(0, 0.2))
